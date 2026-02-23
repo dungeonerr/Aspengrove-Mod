319e07f0576c0a7e1f29c49104e71372f1e8d773
dungeonerr <dungeoneerr@gmail.com>
2026-02-23 19:12:24 +0200

    s

--- src/main/java/dngnrr/aspengrove/Aspengrove.java
+++ src/main/java/dngnrr/aspengrove/Aspengrove.java
@@ -17,6 +17,7 @@ public class Aspengrove implements ModInitializer {
 	@Override
 	public void onInitialize() {
 		ModBlocks.initialize();
+		ModCreativeTab.initialize();
 
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
