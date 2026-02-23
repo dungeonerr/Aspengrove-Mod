562af70d33cabbc6a8a5f49c2d58cb14e94eeec0
dungeonerr <dungeoneerr@gmail.com>
2026-02-23 10:30:33 +0200

    Initial project commit

---
+++ src/client/java/dngnrr/aspengrove/AspengroveClient.java
@@ -0,0 +1,10 @@
+package dngnrr.aspengrove;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AspengroveClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
