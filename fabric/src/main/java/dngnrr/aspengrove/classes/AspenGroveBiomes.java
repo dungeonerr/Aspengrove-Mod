86f85f367fa30f27e2bdf7f54361af9eb1539567
Starman <135715391+StarmanMine142@users.noreply.github.com>
2026-07-15 22:25:41 +0300

    рилиат ты тупорылый еблан, не трогай блять ничего в наших ветках

--- fabric/src/main/java/dngnrr/aspengrove/classes/AspenGroveBiomes.java
+++ fabric/src/main/java/dngnrr/aspengrove/classes/AspenGroveBiomes.java
@@ -1,17 +1,21 @@
-package dngnrr.aspengrove.classes;
-
-import net.minecraft.core.registries.Registries;
-import net.minecraft.resources.Identifier;
-import net.minecraft.resources.ResourceKey;
-import net.minecraft.world.level.biome.Biome;
-
-public class AspenGroveBiomes {
-    public static final ResourceKey<Biome> ASPEN_GROVE = ResourceKey.create(
-            Registries.BIOME,
-            Identifier.fromNamespaceAndPath("aspengrove", "aspen_grove")
-    );
-
-    public static void registerBiomes() {
-        // а пусто нахуй
-    }
-}
+//package dngnrr.aspengrove.classes;
+//
+//import com.terraformersmc.biolith.api.biome.BiomePlacement;
+//import net.minecraft.core.registries.Registries;
+//import net.minecraft.resources.Identifier;
+//import net.minecraft.resources.ResourceKey;
+//import net.minecraft.world.level.biome.Biome;
+//import net.minecraft.world.level.biome.Biomes;
+//
+//public class AspenGroveBiomes {
+//    public static final ResourceKey<Biome> ASPEN_GROVE = ResourceKey.create(
+//            Registries.BIOME,
+//            Identifier.fromNamespaceAndPath("aspengrove", "aspen_grove")
+//    );
+//
+//    public static void registerBiomes() {
+//        BiomePlacement.replaceOverworld(Biomes.FOREST, ASPEN_GROVE, 0.27);
+//        BiomePlacement.replaceOverworld(Biomes.BIRCH_FOREST, ASPEN_GROVE, 0.27);
+//        BiomePlacement.replaceOverworld(Biomes.TAIGA, ASPEN_GROVE, 0.27);
+//    }
+//}
